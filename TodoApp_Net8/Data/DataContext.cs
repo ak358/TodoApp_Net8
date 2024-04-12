f8a0703a0c80553b7348990fb53bdd2facc14e7f
ak358 <arikana.358code@gmail.com>
2024-04-12 10:59:12 +0900

    プロジェクト ファイルを追加します。

---
+++ TodoApp_Net8/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoApp_Net8.Models;
+
+namespace TodoApp_Net8.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+        public DbSet<Todo> Todoes { get; set; }
+
+
+
+
+    }
+}
